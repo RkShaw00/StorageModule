21fb52861d0dbea5acbdcce0824904f802c20dc4
RkShaw00 <117458242+RkShaw00@users.noreply.github.com>
2023-01-12 15:14:35 +0530

    Update provider.tf
    statefile

--- provider.tf
+++ provider.tf
@@ -3,3 +3,13 @@ provider "azurerm" {
     
   }
 }
+
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "aks-cluster"
+    storage_account_name = "demogaiastorageaccount"
+    container_name       = "tfstate"
+    key                  = "demo.terraform.tfstate"
+    #key - (Required) The name of the Blob used to retrieve/store Terraform's State file inside the Storage Container
+  }
+}
