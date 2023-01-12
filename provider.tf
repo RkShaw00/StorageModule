3fe4a7dd55b03d9dfd0e3d70173b21b944eb3ac9
RkShaw00 <117458242+RkShaw00@users.noreply.github.com>
2023-01-12 17:59:13 +0530

    Update provider.tf
    remove backend

--- provider.tf
+++ provider.tf
@@ -4,12 +4,3 @@ provider "azurerm" {
   }
 }
 
-terraform {
-  backend "azurerm" {
-    resource_group_name  = "aks-cluster"
-    storage_account_name = "demogaiastorageaccount"
-    container_name       = "tfstate"
-    key                  = "demo.terraform.tfstate"
-    #key - (Required) The name of the Blob used to retrieve/store Terraform's State file inside the Storage Container
-  }
-}
